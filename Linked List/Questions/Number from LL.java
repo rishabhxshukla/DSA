42f8500d27de0b2439c929f0ad99f864d6eecefa
Rishabh Shukla <rs515666@gmail.com>
2023-03-29 02:16:37 +0530

    Added new files

---
+++ Linked List/Questions/Number from LL.java
@@ -0,0 +1,78 @@
+package Questions;
+import java.io.*;
+
+class Number_from_LL
+{
+    static Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    /* Printing the LL */
+    void printLL()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+
+    /* Creating the LL */
+    void add(int d)
+    {
+        Node new_node = new Node(d);
+        new_node.next = head;
+        head = new_node;
+    }
+
+
+    /* Function to find number made from entire LL */
+    static int number()
+    {
+        Node temp = head;
+        String num = "";
+
+        while (temp != null)
+        {
+            num = num + temp.data;
+            temp = temp.next;
+        }
+
+        //Converting string to integer
+        int ans = Integer.parseInt(num);
+        return ans;
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        /* Creating object of the class */
+        Number_from_LL ll = new Number_from_LL();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        for (int i=0; i<5; i++)
+        {
+            System.out.print("Enter the node : ");
+            int d = Integer.parseInt(b.readLine());
+            ll.add(d);
+        }
+
+        System.out.println("Created Linked List : ");
+        ll.printLL();
+
+        System.out.printf("\nNumber created from LL is : " + number());
+    }
+}
