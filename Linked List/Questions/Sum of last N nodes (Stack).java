2cf3e117d112a354f8aff06078b7b82676b04856
Rishabh Shukla <rs515666@gmail.com>
2023-03-29 01:20:48 +0530

    Added new files

--- Linked List/Questions/Sum of last N nodes (Stack).java
+++ Linked List/Questions/Sum of last N nodes (Stack).java
@@ -1,7 +1,8 @@
 package Questions;
 import java.io.*;
+import java.util.*;
 
-class Sum_Last_N_Nodes
+class Sum_Last_N_Nodes2
 {
     static Node head;
     static class Node
@@ -56,34 +57,32 @@ class Sum_Last_N_Nodes
     /* Function to find sum of last N nodes */
     static int sum(int N)
     {
-        //1 -> 2 -> 3 -> 4 -> 5
+        //Creating a stack
+        Stack<Integer> stack = new Stack<>();
         Node temp = head;
-        int index = lengthOfLL() - N + 1;   //5-3+1=3
-        int count = 1, sum = 0;
+        int sum = 0;
 
+        //Storing all nodes in the stack
         while (temp != null)
         {
-            if (count == index)
-            {
-                while (temp != null)
-                {
-                    sum = sum + temp.data;
-                    temp = temp.next;
-                }
-                return sum;
-            }
-
+            stack.push(temp.data);
             temp = temp.next;
-            count++;
         }
-        return 0;
+
+        //Taking out nodes from stack and calculating the sum
+        for (int i=0; i<N; i++)
+        {
+            sum = sum + stack.peek();
+            stack.pop();
+        }
+        return sum;
     }
 
 
     public static void main(String args[]) throws IOException
     {
         /* Creating object of the class */
-        Sum_Last_N_Nodes ll = new Sum_Last_N_Nodes();
+        Sum_Last_N_Nodes2 ll = new Sum_Last_N_Nodes2();
         InputStreamReader inp = new InputStreamReader(System.in);
         BufferedReader b = new BufferedReader(inp);
 
