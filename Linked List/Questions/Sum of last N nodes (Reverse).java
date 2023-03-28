6c33695666d8d88dba1b071c003c4537a9bc420f
Rishabh Shukla <rs515666@gmail.com>
2023-03-29 01:32:46 +0530

    Added new files

---
+++ Linked List/Questions/Sum of last N nodes (Reverse).java
@@ -0,0 +1,103 @@
+package Questions;
+import java.io.*;
+
+class Sum_Last_N_Nodes3
+{
+    static Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    /* Printing the LL */
+    void printLL()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+
+    /* Creating the LL */
+    void add(int d)
+    {
+        Node new_node = new Node(d);
+        new_node.next = head;
+        head = new_node;
+    }
+
+
+    /* Function to reverse the LL */
+    void reverse()
+    {
+        Node prev = null;
+        Node temp = head;
+        Node next = null;
+
+        while (temp != null)
+        {
+            next = temp.next;
+            temp.next = prev;
+
+            prev = temp;
+            temp = next;
+        }
+        //Changing head node
+        head = prev;
+    }
+
+
+    /* Function to find sum of last N nodes */
+    static int sum(int N)
+    {
+        //Creating object & calling reverse function
+        Sum_Last_N_Nodes3 ll = new Sum_Last_N_Nodes3();
+        ll.reverse();
+
+        Node temp = head;
+        int count = 0, sum = 0;
+
+        while (count != N)
+        {
+            sum = sum + temp.data;
+            temp = temp.next;
+            count++;
+        }
+        return sum;
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        /* Creating object of the class */
+        Sum_Last_N_Nodes3 ll = new Sum_Last_N_Nodes3();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        for (int i=0; i<5; i++)
+        {
+            System.out.print("Enter the node : ");
+            int d = Integer.parseInt(b.readLine());
+            ll.add(d);
+        }
+
+        System.out.println("Created Linked List : ");
+        ll.printLL();
+
+        System.out.print("\nEnter the value of N : ");
+        int N = Integer.parseInt(b.readLine());
+
+        System.out.print("Sum of last n nodes : " + sum(N));
+    }
+}
