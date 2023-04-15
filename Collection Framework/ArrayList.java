b8220a192c9a6632b159f3fd7e957d1ef504c52f
Rishabh Shukla <rs515666@gmail.com>
2023-04-15 19:15:04 +0530

    Made some changes

---
+++ Collection Framework/ArrayList.java
@@ -0,0 +1,10 @@
+package CollectionFramework;
+import java.util.ArrayList;
+
+class Array_List
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
--- Collection Framework/HashMap.java
+++ Collection Framework/HashMap.java
@@ -1,26 +1,26 @@
-package HashMap;
+package CollectionFramework;
 import java.util.HashMap;
 
 class Hash_Map
 {
     public static void main(String[] args) 
     {
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> hm = new HashMap<>();
 
         /* Inserting the records */
-        map.put("Rishabh", 12014745);
-        map.put("Tanya", 12014604);
-        map.put("Anuj", 12015005);
-        map.put("Cristiano", 12017777);
-        map.put("Messi", 12011010);
+        hm.put("Rishabh", 12014745);
+        hm.put("Tanya", 12014604);
+        hm.put("Anuj", 12015005);
+        hm.put("Cristiano", 12017777);
+        hm.put("Messi", 12011010);
 
         /* Printing the Hash table */
-        System.out.println(map);
+        System.out.println(hm);
         System.out.println();
 
 
         /* Getting the id of Rishabh */
-        int id = map.get("Rishabh");
+        int id = hm.get("Rishabh");
         System.out.println(id);
         System.out.println();
 
@@ -28,57 +28,57 @@ class Hash_Map
         /* Checking if our map contains a certain Key */
         boolean isPresent;
 
-        isPresent = map.containsKey("Tanya");
+        isPresent = hm.containsKey("Tanya");
         System.out.println((isPresent));
 
-        isPresent = map.containsKey("Cat");
+        isPresent = hm.containsKey("Cat");
         System.out.println(isPresent);
         System.out.println();
 
 
         /* Checking if our map contains a certain Value */
-        isPresent = map.containsValue(12014745);
+        isPresent = hm.containsValue(12014745);
         System.out.println(isPresent);
         System.out.println();
 
 
         /* To update an existing value, simply use put() function which will overwrite it */
-        map.put("Rishabh", 12013333);
-        System.out.println(map.get("Rishabh"));
+        hm.put("Rishabh", 12013333);
+        System.out.println(hm.get("Rishabh"));
         System.out.println();
 
 
         /* Replace a value in the Hash Map */
-        map.replace("Tanya", 12014604, 12016666);
-        System.out.println(map.get("Tanya"));
+        hm.replace("Tanya", 12014604, 12016666);
+        System.out.println(hm.get("Tanya"));
         System.out.println();
 
 
         /* Put a key-value pair if it is not yet present */
-        map.putIfAbsent("AbsentKey", 10101010);
-        System.out.println(map);
+        hm.putIfAbsent("AbsentKey", 10101010);
+        System.out.println(hm);
         System.out.println();
 
 
         /* Remove a kay-value pair from the Hash Map */
-        map.remove("Anuj");
-        System.out.println(map);
+        hm.remove("Anuj");
+        System.out.println(hm);
         System.out.println();
 
 
         /* Get the size of a Hash Map */
-        int size = map.size();
+        int size = hm.size();
         System.out.println(size);
         System.out.println();
 
         
         /* List all the Keys */
-        System.out.println(map.keySet());
+        System.out.println(hm.keySet());
         System.out.println();
 
 
         /* List all the Values */
-        System.out.println(map.values());
+        System.out.println(hm.values());
         System.out.println();
     }
 }
