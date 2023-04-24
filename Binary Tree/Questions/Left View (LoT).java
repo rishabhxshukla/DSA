7629a1e95a2bd204669cc89f224574f517833604
Rishabh Shukla <rs515666@gmail.com>
2023-04-25 01:44:07 +0530

    Added new files

---
+++ Binary Tree/Questions/Left View (LoT).java
@@ -0,0 +1,102 @@
+package Questions;
+import java.util.*;
+
+class LeftView2
+{
+    static Node root;
+    static class Node
+    {
+        int data;
+        Node left, right;
+
+        Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+
+    /* Creating the BT */
+    void create()
+    {
+        Node first  = new Node(1);
+        Node second = new Node(2);
+        Node third  = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth  = new Node(5);
+
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+        second.right = fifth;
+    }
+
+
+    /* Printing the BT */
+    void print()
+    {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty())
+        {
+            Node temp = q.remove();
+            System.out.print(temp.data + " ");
+
+            if (temp.left != null) {
+                q.add(temp.left);
+            }
+            if (temp.right != null) {
+                q.add(temp.right);
+            }
+        }
+    }
+
+
+    /* Function to print the left view of BT */
+    void leftView()
+    {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty())
+        {
+            //Traversing all nodes of current level
+            for (int i=0; i<q.size(); i++)
+            {
+                Node temp = q.remove();
+                //Printing the first element only
+                if (i == 0) {
+                    System.out.print(temp.data + " ");
+                }
+
+                if (temp.left != null) {
+                    q.add(temp.left);
+                }
+                if (temp.right != null) {
+                    q.add(temp.right);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String args[])
+    {
+        //Creating object of the class
+        LeftView2 t = new LeftView2();
+
+        //Creating the BT
+        t.create();
+
+        //Printing the BT
+        System.out.println("*** Created Binary Tree ***");
+        t.print();
+
+        //Printing left view of BT
+        System.out.println("\nLeft View Of Binary Tree : ");
+        t.leftView();
+    }
+}
