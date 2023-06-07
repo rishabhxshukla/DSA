926c7bcd8fce01e1a00f6077721673ab11fc1eab
Rishabh Shukla <rs515666@gmail.com>
2023-06-08 00:16:21 +0530

    Added new files

---
+++ Array/Questions/Array Sum.java
@@ -0,0 +1,46 @@
+package Questions;
+import java.io.*;
+
+class ArraySum
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to find the sum of the array */
+    static int sum()
+    {
+        int sum = 0;
+
+        for (int i=0; i<size; i++)
+        {
+            sum = sum + arr[i];
+        }
+
+        return sum;
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        //Printing the sum
+        System.out.println("\nSum : " + sum());
+    }
+}
