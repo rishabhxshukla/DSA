f42e2d7313b60f230e9f2464cc1660c31b7cc896
Rishabh Shukla <rs515666@gmail.com>
2023-06-08 15:54:49 +0530

    Added new files

---
+++ Array/Boilerplate.java
@@ -0,0 +1,37 @@
+// ...
+
+package Questions;
+import java.io.*;
+
+class Boilerplate
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to find the ... */
+    //
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        //Printing the ...
+    }
+}
