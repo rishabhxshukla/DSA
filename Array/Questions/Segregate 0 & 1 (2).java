976c050926820c4d247f2000ac7a73308de3b6b1
Rishabh Shukla <rs515666@gmail.com>
2023-06-10 21:13:39 +0530

    Added new files

---
+++ Array/Questions/Segregate 0 & 1 (2).java
@@ -0,0 +1,68 @@
+// Segregate 0s on left side and 1s on right side of the array
+
+package Questions;
+import java.io.*;
+
+class Segregate2
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to segregate the array */
+    void segregate()
+    {
+        int count = 0;
+
+        //Counting the no. of 0s
+        for (int i=0; i<size; i++)
+        {
+            if (arr[i] == 0)
+                count++;
+        }
+
+        //Filling array with 0s until count
+        for (int i=0; i<count; i++)
+        {
+            arr[i] = 0;
+        }
+
+        //Filling the remaining array with 1s
+        for (int i=count; i<size; i++)
+        {
+            arr[i] = 1;
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        Segregate2 obj = new Segregate2();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Original Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        //Segregating the array
+        obj.segregate();
+
+        //Printing the segregated array
+        System.out.println("\n*** New Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
