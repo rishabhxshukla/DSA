295ca56e5804866e3e6f4799570e8d19cac552c6
Rishabh Shukla <rs515666@gmail.com>
2023-06-11 16:15:40 +0530

    Added new files

---
+++ Array/Questions/Dutch National Flag.java
@@ -0,0 +1,76 @@
+/*
+Dutch national flag consists of three colors: red, white, and blue.
+We are given an array with these colours arranged randomly.
+Our task is to put the same colours together and in the correct order.
+*/
+
+package Array.Questions;
+import java.io.*;
+
+class DutchFlag
+{
+    static String arr[] = {"white", "red", "white", "blue" ,"red", "blue"};
+    static int size = arr.length;
+
+
+    /* Function to make the Dutch flag */
+    void makeFlag()
+    {
+        int red = 0, white = 0, blue = 0;
+
+        //Counting the no. of colours
+        for (int i=0; i<size; i++)
+        {
+            if (arr[i] == "red")
+                red++;
+            else if (arr[i] == "white")
+                white++;
+            else if (arr[i] == "blue")
+                blue++;
+        }
+
+        //Making the flag
+        int i = 0;
+        while (red > 0)
+        {
+            arr[i++] = "red";
+            red--;
+        }
+        while (white > 0)
+        {
+            arr[i++] = "white";
+            white--;
+        }
+        while (blue > 0)
+        {
+            arr[i++] = "blue";
+            blue--;
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        DutchFlag obj = new DutchFlag();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        //Making the flag
+        obj.makeFlag();
+
+        //Printing the flag
+        System.out.println("\n\n*** Dutch National Flag ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
