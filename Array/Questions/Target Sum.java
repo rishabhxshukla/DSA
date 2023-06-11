f9d599dc298faf1227841e9a1d5b186481af8a09
Rishabh Shukla <rs515666@gmail.com>
2023-06-12 03:08:41 +0530

    Added new files

---
+++ Array/Questions/Target Sum.java
@@ -0,0 +1,73 @@
+// Target sum problem
+
+package Array.Questions;
+import java.io.*;
+import java.util.*;
+
+class TargetSum
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to find the pairs whose sum is equal to target */
+    void pairs(int target)
+    {
+        //ArrayList to store the pairs
+        ArrayList<Integer> pair = new ArrayList<>();
+
+        for (int i=0; i<size-1; i++)
+        {
+            for (int j=i+1; j<size; j++)
+            {
+                if (arr[i] + arr[j] == target)
+                {
+                    pair.add(arr[i]);
+                    pair.add(arr[j]);
+                }
+            }
+        }
+
+        //Printing the pairs
+        for (int i=0; i<pair.size(); i=i+2)
+        {
+            int e1 = pair.get(i);
+            int e2 = pair.get(i+1);
+            System.out.print("(" + e1 + "," + e2 + ")" + " ");
+        }
+
+        //Deleting the ArrayList
+        pair.clear();
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        TargetSum obj = new TargetSum();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        //Taking user input
+        System.out.print("\nEnter the target : ");
+        int target = Integer.parseInt(b.readLine());
+
+        //Printing the pairs
+        System.out.println("*** Pairs ***");
+        obj.pairs(target);
+    }
+}
