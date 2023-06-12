c351ac006bbe6263873de72e54b54fabc4a2ffd8
Rishabh Shukla <rs515666@gmail.com>
2023-06-12 15:45:08 +0530

    Added new files

---
+++ Recursion/Print Array.java
@@ -0,0 +1,41 @@
+package Recursion;
+import java.io.*;
+
+class PrintArray
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to print the array */
+    void print(int i)
+    {
+        if (i == size-1)
+            System.out.print(arr[i]);
+        else
+        {
+            System.out.print(arr[i] + " ");
+            print(i+1);
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        PrintArray obj = new PrintArray();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        obj.print(0);
+    }
+}
