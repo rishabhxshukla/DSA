3515b04ab9646dd943ac47bb90fd6486dba8aba0
Rishabh Shukla <rs515666@gmail.com>
2023-06-12 15:52:23 +0530

    Added new files

---
+++ Recursion/Print Array Reverse.java
@@ -0,0 +1,41 @@
+package Recursion;
+import java.io.*;
+
+class PrintArrayReverse
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to print the reverse of array */
+    void print(int i)
+    {
+        if (i == 0)
+            System.out.print(arr[i]);
+        else
+        {
+            System.out.print(arr[i] + " ");
+            print(i-1);
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        PrintArrayReverse obj = new PrintArrayReverse();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Reversed Array ***");
+        obj.print(size-1);
+    }
+}
