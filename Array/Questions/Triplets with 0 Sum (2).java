f258ae2dc254b2a96ed0de3b62d046c3687625e8
Rishabh Shukla <rs515666@gmail.com>
2023-06-15 04:34:13 +0530

    Added new files

---
+++ Array/Questions/Triplets with 0 Sum (2).java
@@ -0,0 +1,86 @@
+// Find triplets in the array whose sum is zero
+// HASHSET
+
+package Array.Questions;
+import java.io.*;
+import java.util.*;
+
+class Triplets2
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Creating the array */
+    void create() throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+    }
+
+
+    /* Printing the array */
+    void print(int a[])
+    {
+        for (int i=0; i<a.length; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+
+    // TIME COMPLEXITY : O(N^2)
+    /* Function to find the triplets */
+    void triplets()
+    {
+        HashSet<Integer> hs = new HashSet<>();
+        boolean present = false;
+
+        //Finding the sum of 2 elements
+        for (int i=0; i<size-1; i++)
+        {
+            for (int j=i+1; j<size; j++)
+            {
+                //3rd number
+                int sum = -(arr[i] + arr[j]);
+
+                //If 3rd no. is present in HashSet
+                if (hs.contains(sum)) {
+                    System.out.println("(" + arr[i] + ", " + arr[j] + ", " + sum + ")");
+                    present = true;
+                }
+
+                //Otherwise add 2nd no. to HashSet
+                hs.add(arr[j]);
+            }
+        }
+
+        //If no triplet is present
+        if (!present)
+            System.out.println("Triplets not found!");
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        Triplets2 obj = new Triplets2();
+
+        //Creating the array
+        obj.create();
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        obj.print(arr);
+
+        //Printing the triplets
+        System.out.println("\n*** Triplets ***");
+        obj.triplets();
+    }
+}
