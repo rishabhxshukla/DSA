c37550b2a2a4aa6d7a317eb48fae9202e5b798c8
Rishabh Shukla <rs515666@gmail.com>
2023-06-16 00:27:53 +0530

    Added new files

---
+++ Heap/Questions/Kth Smallest.java
@@ -0,0 +1,72 @@
+// Kth smallest element in an unsorted array
+// MIN HEAP
+
+package Heap.Questions;
+import java.io.*;
+import java.util.PriorityQueue;
+
+class KthSmallest
+{
+    static final int size = 5;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+
+    /* Creating the heap */
+    void create() throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            int e = Integer.parseInt(b.readLine());
+            pq.add(e);
+        }
+    }
+
+
+    /* Printing the heap */
+    void print()
+    {
+        System.out.println(pq);
+    }
+
+
+    /* Function to return the Kth smallest element */
+    static int Kth(int k)
+    {
+        int min = 0;
+
+        while(k > 0)
+        {
+            min = pq.remove();
+            k--;
+        }
+
+        return min;
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        KthSmallest obj = new KthSmallest();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the heap
+        obj.create();
+
+        //Printing the heap
+        System.out.println("*** Created Heap ***");
+        obj.print();
+
+        //Taking user input
+        System.out.print("Enter the value of K : ");
+        int k = Integer.parseInt(b.readLine());
+
+        //Printing the Kth smallest element
+        System.out.println("Kth smallest element : " + Kth(k));
+    }
+}
