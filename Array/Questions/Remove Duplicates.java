787bf635c24d703e26763afff63cddb2962d55b1
Rishabh Shukla <rs515666@gmail.com>
2023-06-21 03:42:55 +0530

    Added new files

---
+++ Array/Questions/Remove Duplicates.java
@@ -0,0 +1,58 @@
+// Remove duplicate elements from a sorted array
+// 1 POINTER
+
+package Array.Questions;
+import java.io.*;
+
+class RemoveDuplicates
+{
+    static int arr[] = {2, 3, 3, 7, 9, 9, 9, 10, 12, 12, 12};
+
+
+    /* Printing the array */
+    void print(int a[])
+    {
+        for (int i=0; i<a.length; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+
+    /* Function to remove the duplicates */
+    void removeDuplicates()
+    {
+        //Pointer
+        int k = 0;
+
+        for (int i=1; i<arr.length; i++)
+        {
+            if (arr[i] != arr[k]) {
+                k++;
+                arr[k] = arr[i];
+            }
+        }
+
+        //Printing the elements after removing duplicates
+        for (int i=0; i<k; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        RemoveDuplicates obj = new RemoveDuplicates();
+
+        //Printing original array
+        System.out.println("*** Original Array ***");
+        obj.print(arr);
+
+        //Removing duplicates
+        System.out.println("\nRemoving duplicates...");
+        System.out.println("*** New Array ***");
+        obj.removeDuplicates();
+    }
+}
