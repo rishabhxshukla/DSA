fc2a8679017e0de609204965047fd280b64ea42e
Rishabh Shukla <rs515666@gmail.com>
2023-06-20 21:37:55 +0530

    Added new files

---
+++ Recursion/Number Palindrome.java
@@ -0,0 +1,37 @@
+// Program to check whether a number is palindrome or not
+
+package Recursion;
+import java.io.*;
+
+class NumberPalindrome
+{
+    /* Function to check for a palindrome number */
+    static int sum = 0;
+    static int palindrome(int num)
+    {
+        if (num == 0) {
+            return sum;
+        }
+
+        //Storing the reverse of num
+        int r = num % 10;
+        sum = (sum * 10) + r;
+
+        return palindrome(num / 10);
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the number : ");
+        int num = Integer.parseInt(b.readLine());
+
+        //Printing the result
+        boolean ans = num == palindrome(num);
+        System.out.println("Palindrome : " + ans);
+    }
+}
