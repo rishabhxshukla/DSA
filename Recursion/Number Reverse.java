2e37f5ff02c7180a00f1b4ee4e5b4c38ff9c6dfd
Rishabh Shukla <rs515666@gmail.com>
2023-06-21 02:43:59 +0530

    Added new files

---
+++ Recursion/Number Reverse.java
@@ -0,0 +1,36 @@
+// Program to reverse a number
+
+package Recursion;
+import java.io.*;
+
+class NumberReverse
+{
+    /* Function to reverse a number */
+    static int sum = 0;
+    static int reverse(int num)
+    {
+        if (num == 0) {
+            return sum;
+        }
+
+        //Storing the reverse of num
+        int r = num % 10;
+        sum = (sum * 10) + r;
+
+        return reverse(num / 10);
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the number : ");
+        int num = Integer.parseInt(b.readLine());
+
+        //Printing the reversed number
+        System.out.print("Reversed number : " + reverse(num));
+    }
+}
