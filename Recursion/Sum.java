87ec988baa6bb1f5797a686d83289ef315d52d53
Rishabh Shukla <rs515666@gmail.com>
2023-06-26 15:57:46 +0530

    Added new files

---
+++ Recursion/Sum.java
@@ -0,0 +1,33 @@
+// Program to find the sum of first n natural numbers
+
+package Recursion;
+import java.io.*;
+
+class Sum
+{
+    /* Function to find the sum */
+    static int sum = 0;
+    static int sum(int n)
+    {
+        if (n == 0) {
+            return sum;
+        }
+
+        sum = n + sum(n - 1);
+        return sum;
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the value of N : ");
+        int n = Integer.parseInt(b.readLine());
+
+        //Printing the sum
+        System.out.println("Sum : " + sum(n));
+    }
+}
