3c065e1a084c540e5b50bb605d3eb80c161f5159
Rishabh Shukla <rs515666@gmail.com>
2023-06-27 03:16:30 +0530

    Added new files

---
+++ Heap/Questions/K Non-Repeating.java
@@ -0,0 +1,86 @@
+// Find first `k` non-repeating characters in a string
+
+package Heap.Questions;
+import java.io.*;
+import java.util.*;
+
+class KNonRepeating
+{
+    static PriorityQueue<Character> pq = new PriorityQueue<>();
+
+
+    /* Creating the heap */
+    void create() throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        System.out.print("Enter the string : ");
+        String str = b.readLine();
+        str = str.toUpperCase();
+
+        for (int i=0; i<str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            pq.add(ch);
+        }
+    }
+
+
+    /* Printing the heap */
+    void print()
+    {
+        System.out.println(pq);
+    }
+
+
+    /* Function to find the first k non-repeating characters */
+    void nonRepeating(int k)
+    {
+        while (k > 0 && k < pq.size())
+        {
+            //Removing root character
+            char removed = pq.remove();
+
+            //If next character is not same as root
+            if (removed != pq.peek())
+            {
+                System.out.print(removed + " ");
+                //Decreasing counter
+                k--;
+            }
+            else
+            {
+                //Removing next characters till they are not same
+                while (removed == pq.peek())
+                {
+                    pq.remove();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        KNonRepeating obj = new KNonRepeating();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the heap
+        obj.create();
+
+        //Printing the heap
+        System.out.println("*** Created Heap ***");
+        obj.print();
+
+        //Taking user input
+        System.out.print("Enter the value of K : ");
+        int k = Integer.parseInt(b.readLine());
+
+        //Printing the k non-repeating characters
+        System.out.println("First k non-repeating characters :");
+        obj.nonRepeating(k);
+    }
+}
