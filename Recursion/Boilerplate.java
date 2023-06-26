7b4514096dd00a70f49a4908e7e50f9a363ae48f
Rishabh Shukla <rs515666@gmail.com>
2023-06-26 16:23:51 +0530

    Added new files

---
+++ Recursion/Boilerplate.java
@@ -0,0 +1,23 @@
+// Program to find the ...
+
+package Recursion.Questions;
+import java.io.*;
+
+class Boilerplate
+{
+    /* Function to find the ... */
+    //
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the value of N : ");
+        int n = Integer.parseInt(b.readLine());
+
+        //Printing the ...
+    }
+}
