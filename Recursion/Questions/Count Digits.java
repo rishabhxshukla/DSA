1b396559bc5efa3c34429b3ea9d0cfd58f3ad850
Rishabh Shukla <rs515666@gmail.com>
2023-06-26 18:29:46 +0530

    Added new files

---
+++ Recursion/Questions/Count Digits.java
@@ -0,0 +1,36 @@
+// Program to count the digits of a given number
+
+package Recursion.Questions;
+import java.io.*;
+
+class CountDigits
+{
+    /* Function to find the count */
+    static int count = 0;
+    static int count(int num)
+    {
+        if (num == 0) {
+            return count;
+        }
+
+        //Finding digits and increasing count
+        int r = num % 10;
+        count++;
+
+        return count(num / 10);
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the number : ");
+        int num = Integer.parseInt(b.readLine());
+
+        //Printing the count
+        System.out.println("Count of digits : " + count(num));
+    }
+}
