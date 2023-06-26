71d7b8ea8638c8e96b55f961141cd3db62d5417a
Rishabh Shukla <rs515666@gmail.com>
2023-06-26 18:38:20 +0530

    Added new files

---
+++ Recursion/Questions/Array Max.java
@@ -0,0 +1,52 @@
+// Program to find the largest element of an array
+
+package Recursion.Questions;
+import java.io.*;
+
+class ArrayMax
+{
+    static final int size = 5;
+    static int arr[] = new int[size];
+
+
+    /* Function to find the max element */
+    static int max = Integer.MIN_VALUE;
+    static int max(int i)
+    {
+        if (i == arr.length) {
+            return max;
+        }
+
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+
+        return max(i + 1);
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        ArrayMax obj = new ArrayMax();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Creating the array
+        for (int i=0; i<size; i++)
+        {
+            System.out.print("Enter the element : ");
+            arr[i] = Integer.parseInt(b.readLine());
+        }
+
+        //Printing the array
+        System.out.println("*** Created Array ***");
+        for (int i=0; i<size; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        //Finding the largest element
+        System.out.println("\nMax : " + max(0));
+    }
+}
