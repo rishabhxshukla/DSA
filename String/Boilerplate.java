857cc9bb3f843fe070ad45b85075e6e325f83dce
Rishabh Shukla <rs515666@gmail.com>
2023-07-06 18:00:56 +0530

    Added new files

---
+++ String/Boilerplate.java
@@ -0,0 +1,25 @@
+//
+
+package String;
+import java.io.*;
+
+class Boilerplate
+{
+    /* Function to ... */
+    //
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        Boilerplate obj = new Boilerplate();
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the string : ");
+        String str = b.readLine();
+
+        //...
+    }
+}
