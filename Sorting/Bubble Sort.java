b30129761cfbf4165c5a6630e65422be2d38bbc3
Rishabh Shukla <rs515666@gmail.com>
2024-05-12 16:06:28 +0530

    Made some changes

--- Sorting/Bubble Sort.java
+++ Sorting/Bubble Sort.java
@@ -44,18 +44,18 @@ class BubbleSort
     /* Function to sort elements of an array */
     void sort()
     {
-        int n = size - 1;
+        int n = size;
 
-        for (int i=0; i<n; i++)
+        for (int i = 0; i < (n-1); i++)
         {
-            for (int j=0; j<(n-i); j++)
+            for (int j = 0; j < (n-i-1); j++)
             {
-                if (arr[j] > arr[j+1])
+                if (arr[j] > arr[j + 1])
                 {
                     //Swapping
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
--- Sorting/Selection Sort.java
+++ Sorting/Selection Sort.java
@@ -43,16 +43,17 @@ class SelectionSort
     {
         int n = size;
 
-        for (int i=0; i<(n-1); i++)
+        for (int i = 0; i < (n-1); i++)
         {
             int min = i;
 
-            for (int j=(i+1); j<n; j++)
+            for (int j = (i+1); j < n; j++)
             {
                 if (arr[j] < arr[min]) {
                     min = j;
                 }
             }
+
             //Swapping
             int temp = arr[i];
             arr[i] = arr[min];
