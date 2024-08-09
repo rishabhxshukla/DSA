b6fe5eb941b181b9ac083fedc54cd0439336159f
Rishabh Shukla <rs515666@gmail.com>
2024-08-09 16:47:23 +0530

    Added new files

---
+++ Patterns/2.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+class Pattern2
+{
+    /* Function to create the pattern */
+    void create(int N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        //Creating object of the class
+        Pattern2 obj = new Pattern2();
+
+        //Creating the pattern
+        obj.create(5);
+    }
+}
