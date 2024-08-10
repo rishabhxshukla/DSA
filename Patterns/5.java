d869a760e50a963d4940b41fc43f3ef8924f55ad
Rishabh Shukla <rs515666@gmail.com>
2024-08-10 16:18:04 +0530

    Added new files

---
+++ Patterns/5.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+class Pattern5
+{
+    /* Function to create the pattern */
+    void create(int N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = N; j > i; j--)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        //Creating object of the class
+        Pattern5 obj = new Pattern5();
+
+        //Creating the pattern
+        obj.create(5);
+    }
+}
