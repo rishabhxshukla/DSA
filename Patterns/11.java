c8c2ac431d1c6123ec676971b063d0a06aea499b
Rishabh Shukla <rs515666@gmail.com>
2024-08-11 14:34:06 +0530

    Added new files

---
+++ Patterns/11.java
@@ -0,0 +1,38 @@
+package Patterns;
+
+class Pattern11
+{
+    /* Function to create the pattern */
+    void create(int N)
+    {
+        for (int i = 0; i <= (2 * N); i++)
+        {
+            /* Upper Part */
+            if (i < N)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    System.out.print("* ");
+                }
+            }
+            /* Lower Part */
+            else
+            {
+                for (int j = 0; j <= (2 * N - i); j++)
+                {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        //Creating object of the class
+        Pattern11 obj = new Pattern11();
+
+        //Creating the pattern
+        obj.create(5);
+    }
+}
