be4efe48071fe35d751546480485b008902d4246
Rishabh Shukla <rs515666@gmail.com>
2024-12-22 02:59:04 +0530

    Added new files

---
+++ Linked List/Deletion/Doubly Linked List/All.java
@@ -0,0 +1,85 @@
+package LinkedList.DoublyLL.Deletion;
+
+class All
+{
+    static Node head;
+    static class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+
+    /* Creating the LL */
+    void create(int data)
+    {
+        Node new_node = new Node(data);
+
+        if(head == null) {
+            head = new_node;
+            return;
+        }
+
+        Node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+
+        temp.next = new_node;
+        new_node.next = null;
+        new_node.prev = temp;
+    }
+
+
+    /* Printing the LL */
+    void print()
+    {
+        Node temp = head;
+
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+
+    /* Deleting the whole Linked List */
+    void delete()
+    {
+        head = null;
+    }
+
+
+    public static void main(String args[])
+    {
+        /* Creating object of the class */
+        All ll = new All();
+
+        /* Creating the LL */
+        for (int i = 1; i <= 5; i++)
+        {
+            ll.create(i);
+        }
+
+        /* Original LL */
+        System.out.println("*** Original Linked List ***");
+        ll.print();
+
+        /* Deleting full LL */
+        ll.delete();
+
+        /* New LL */
+        System.out.println("\n*** New Linked List ***");
+        ll.print();
+    }
+}
