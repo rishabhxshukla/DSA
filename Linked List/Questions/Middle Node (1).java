7d8ab89e8b002db4765bb72f0f652a133b58fa7d
Rishabh Shukla <rs515666@gmail.com>
2025-01-22 01:02:06 +0530

    Made some changes

--- Linked List/Questions/Middle Node (1).java
+++ Linked List/Questions/Middle Node (1).java
@@ -1,9 +1,10 @@
 // Find the middle node of a linked list
+// LENGTH / 2
 
 package LinkedList.Questions;
 import java.io.*;
 
-class MiddleNode
+class MiddleNode1
 {
     static Node head;
     static class Node
@@ -19,11 +20,33 @@ class MiddleNode
     }
 
 
+    /* Creating the LL */
+    void create(int data)
+    {
+        Node new_node = new Node(data);
+
+        if(head == null) {
+            head = new_node;
+            return;
+        }
+
+        Node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+
+        temp.next = new_node;
+        new_node.next = null;
+    }
+
+
     /* Printing the LL */
     void print()
     {
         Node temp = head;
-        while (temp != null)
+
+        while(temp != null)
         {
             System.out.print(temp.data + " ");
             temp = temp.next;
@@ -31,16 +54,7 @@ class MiddleNode
     }
 
 
-    /* Creating the LL */
-    void add(int d)
-    {
-        Node new_node = new Node(d);
-        new_node.next = head;
-        head = new_node;
-    }
-
-
-    /* Function to find the length of LL */
+    /* Helper function to find the length of LL */
     static int length()
     {
         Node temp = head;
@@ -57,7 +71,7 @@ class MiddleNode
 
 
     /* Function to find the middle node */
-    static int middle()
+    static Node middle()
     {
         Node temp = head;
         int count = 0;
@@ -66,29 +80,27 @@ class MiddleNode
         while (temp != null)
         {
             if (count == mid) {
-                return temp.data;
+                return temp;
             }
             count++;
             temp = temp.next;
         }
 
-        return -1;
+        return null;
     }
 
 
     public static void main(String args[]) throws IOException
     {
         //Creating object of the class
-        MiddleNode ll = new MiddleNode();
+        MiddleNode1 ll = new MiddleNode1();
         InputStreamReader inp = new InputStreamReader(System.in);
         BufferedReader b = new BufferedReader(inp);
 
         //Creating the LL
-        for (int i=0; i<5; i++)
+        for (int i = 1; i <= 5; i++)
         {
-            System.out.print("Enter the node : ");
-            int d = Integer.parseInt(b.readLine());
-            ll.add(d);
+            ll.create(i);
         }
 
         //Printing the LL
@@ -96,6 +108,7 @@ class MiddleNode
         ll.print();
 
         //Printing the middle node
-        System.out.println("\nMiddle node : " + middle());
+        Node ans = middle();
+        System.out.println("\nMiddle node : " + ans.data);
     }
 }
