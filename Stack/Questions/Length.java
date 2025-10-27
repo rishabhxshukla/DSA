3783ca5daea1b0ea85bd2817f5d7e578d3d9e40e
Rishabh Shukla <rs515666@gmail.com>
2025-10-28 01:33:28 +0530

    Made some changes

--- Stack/Questions/Length.java
+++ Stack/Questions/Length.java
@@ -2,40 +2,18 @@
 
 package Stack.Questions;
 import java.io.*;
+import java.util.Stack;
 
 class Length
 {
-    static int top = -1;
-    static final int MAX = 5;
-    int a[] = new int[MAX];
+    static Stack<Integer> s = new Stack<>();
 
-
-    /* Print Stack */
-    void print()
-    {
-        for (int i=top; i>=0; i--)
-        {
-            System.out.print(a[i] + " ");
-        }
-    }
-
-
-    /* PUSH */
-    void push(int data)
-    {
-        if (top > MAX - 1)
-            System.out.println("Stack Overflow!");
-        else
-            a[++top] = data;
-    }
-
-
-    /* Function to find length of stack */
+    /* Function to find the length of stack */
     static int length()
     {
         int count = 0;
 
-        for (int i=top; i>=0; i--)
+        for (int i = 0; i <= s.size(); i++)
         {
             count++;
         }
@@ -43,16 +21,14 @@ class Length
         return count;
     }
 
-
     public static void main(String args[]) throws IOException
     {
         //Creating object of the class
-        Length s = new Length();
         InputStreamReader inp = new InputStreamReader(System.in);
         BufferedReader b = new BufferedReader(inp);
 
         //Creating the stack
-        for (int i=0; i<MAX; i++)
+        for (int i = 0; i < 5; i++)
         {
             System.out.print("Enter the data : ");
             int d = Integer.parseInt(b.readLine());
@@ -60,8 +36,7 @@ class Length
         }
 
         //Printing the stack
-        System.out.println("Created Stack :");
-        s.print();
+        System.out.println("\nCreated Stack : " + s);
 
         //Printing the length of stack
         System.out.println("\nLength of stack is : " + length());
