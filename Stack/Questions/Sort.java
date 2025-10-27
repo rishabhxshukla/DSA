b9f3937fd18c483091023feb36d3a5cf83322fab
Rishabh Shukla <rs515666@gmail.com>
2025-10-28 02:11:01 +0530

    Made some changes

--- Stack/Questions/Sort.java
+++ Stack/Questions/Sort.java
@@ -1,93 +1,35 @@
-/*
-Sort a stack.
-STEPS :
-1) We create a temporary stack. Now we run a loop while the input stack is not empty.
-2) At each iteration, we pop an element from the input stack and compare it with the element
-   present on top of the tempStack.
-3) If tempStack is not empty and the top element of input stack > top element of the tempStack,
-   we pop the top element from the tempStack and push it into the input stack.
-   Otherwise, if the element is smaller, we push it into the tempStack.
-4) The input stack will be empty by the end of the outer loop, and the sorted elements are present in
-   the temporary stack. So we return the tempStack as an output.
-*/
+// Sort a stack without modifying the original one.
 
 package Stack.Questions;
 import java.io.*;
+import java.util.*;
 
 class Sort
 {
-    int top = -1;
-    static final int MAX = 5;
-    int a[] = new int[MAX];
+    static Stack<Integer> s = new Stack<>();
+    static Stack<Integer> ans = new Stack<>();
 
 
-    /* Print Stack */
-    void print()
+    /* Function to sort the given stack */
+    void sort()
     {
-        for (int i=top; i>=0; i--)
+        //Copying original stack into temp stack
+        Stack<Integer> temp = new Stack<>();
+        temp.addAll(s);
+
+        while (!temp.isEmpty())
         {
-            System.out.print(a[i] + " ");
-        }
-    }
+            //Top element from temp
+            int stkTop = temp.pop();
 
-
-    /* isEmpty */
-    boolean isEmpty()
-    {
-        return (top < 0);
-    }
-
-
-    /* PUSH */
-    void push(int data)
-    {
-        if (top > MAX - 1)
-            System.out.println("Stack Overflow!");
-        else
-            a[++top] = data;
-    }
-
-
-    /* POP */
-    int pop()
-    {
-        if (top < 0) {
-            System.out.println("Stack Underflow!");
-            return -999;
-        }
-        else
-            return a[top--];
-    }
-
-
-    /* PEEK */
-    int peek()
-    {
-        if (top < 0) {
-            System.out.println("Stack is Empty!");
-            return -999;
-        }
-        else
-            return a[top];
-    }
-
-
-    /* Function to sort a given stack */
-    void sort(Sort s, Sort t)
-    {
-        while (!s.isEmpty())
-        {
-            //Top of original stack
-            int stkTop = s.pop();
-
-            while (!t.isEmpty() && stkTop > t.peek())
+            while (!ans.isEmpty() && stkTop < ans.peek())
             {
-                //Popping element from tempStack and pushing into input stack
-                s.push(t.pop());
+                //Move elements from sorted stack back to temp if they are larger
+                temp.push(ans.pop());
             }
 
-            //Otherwise, if element is smaller, we push it into the tempStack
-            t.push(stkTop);
+            //Otherwise, insert the current element into the correct position
+            ans.push(stkTop);
         }
     }
 
@@ -95,29 +37,28 @@ class Sort
     public static void main(String args[]) throws IOException
     {
         //Creating objects of the class
-        Sort stack = new Sort();
-        Sort tempStack = new Sort();
+        Sort obj = new Sort();
         InputStreamReader inp = new InputStreamReader(System.in);
         BufferedReader b = new BufferedReader(inp);
 
         //Creating the stack
-        for (int i=0; i<MAX; i++)
+        for (int i=0; i<5; i++)
         {
             System.out.print("Enter the data : ");
             int d = Integer.parseInt(b.readLine());
-            stack.push(d);
+            s.push(d);
         }
 
         //Printing original stack
-        System.out.println("*** Original Stack ***");
-        stack.print();
+        System.out.println("\n*** Original Stack ***");
+        System.out.println(s);
 
         //Sorting the stack
         System.out.println("\nSorting the Stack...");
-        stack.sort(stack, tempStack);
+        obj.sort();
 
         //Printing new stack
-        System.out.println("*** New Stack ***");
-        tempStack.print();
+        System.out.println("\n*** New Stack ***");
+        System.out.println(ans);
     }
 }
