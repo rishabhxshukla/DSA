39a0b49656ef0b8f4a9326094d6588915b156e0f
Rishabh Shukla <rs515666@gmail.com>
2026-02-07 21:34:44 +0530

    Added new files

---
+++ Array/Questions/Max Vowels in Substring (1).java
@@ -0,0 +1,78 @@
+/*
+Maximum number of vowels in a substring
+Given a string str and an integer k, return the maximum number of vowel letters in any substring of str with length k.
+Vowel letters in English are a, e, i, o, u.
+
+Example 1 :
+Input: str = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+
+Example 2 :
+Input: str = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+
+Example 3 :
+Input: str = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+*/
+// NESTED LOOPS -> SLIDING WINDOW -> O(n*k)
+
+package Array.Questions;
+import java.io.*;
+import java.util.*;
+
+class MaxVowelsInSubstring1
+{
+    static String str = "leetcode";
+    static int k = 3;
+
+
+    /* Helper function to check if a character is vowel or not */
+    static boolean isVowel(char ch)
+    {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+
+
+    /* Function to find the maximum number of vowels in the substring */
+    static int maxVowels()
+    {
+        int max = 0;
+
+        for (int i = 0; i <= (str.length() - k); i++)
+        {
+            //Reinitialize count to 0 for every window
+            int count = 0;
+
+            //Sliding window of size k starting from ith index
+            for (int j = i; j < (i + k); j++)
+            {
+                char ch = str.charAt(j);
+
+                if (isVowel(ch)) {
+                    count++;
+                }
+            }
+
+            //Update max count
+            if (count > max) {
+                max = count;
+            }
+        }
+
+        return max;
+    }
+
+
+    public static void main(String args[])
+    {
+        //Printing the input string
+        System.out.println("String : " + str);
+
+        //Printing the result
+        System.out.println("Max vowels : " + maxVowels());
+    }
+}
