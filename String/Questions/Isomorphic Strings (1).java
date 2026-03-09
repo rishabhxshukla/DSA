0b97fa6e912d27b8b7d26be6f42b3c8f738d7bd4
Rishabh Shukla <rs515666@gmail.com>
2026-03-09 21:27:29 +0530

    Added new files

---
+++ String/Questions/Isomorphic Strings (1).java
@@ -0,0 +1,77 @@
+/*
+Given two strings s and t of same length, determine if they are isomorphic or not.
+Two strings are isomorphic if the characters in s can be replaced to get t.
+No two characters may map to the same character, but a character may map to itself.
+
+Example 1 :
+Input: s = "dad", t = "mom"
+Output: true
+Explanation:
+The strings s and t can be made identical by:
+    Mapping 'd' to 'm'.
+    Mapping 'a' to 'o'.
+
+Example 2 :
+Input: s = "a11", t = "b23"
+Output: false
+Explanation:
+The strings s and t can not be made identical as '1' needs to be mapped to both '2' and '3'.
+*/
+// 1 HASHMAP (one-way mapping + reverse check)
+// O(N)² because hm.containsValue() is O(N)
+
+package String.Questions;
+import java.io.*;
+import java.util.*;
+
+class IsomorphicStrings1
+{
+    /* Function to check if the given strings are isomorphic */
+    static boolean isIsomorphic(String str1, String str2)
+    {
+        HashMap<Character, Character> hm = new HashMap<>();
+
+        for (int i = 0; i < str1.length(); i++)
+        {
+            //Extracting characters from both strings
+            char ch1 = str1.charAt(i);
+            char ch2 = str2.charAt(i);
+
+            //Forward mapping :
+            //If HashMap contains the key, its value must match with ch2
+            if (hm.containsKey(ch1) && hm.get(ch1) != ch2) {
+                return false;
+            }
+
+            //Reverse mapping :
+            //If ch2 is already mapped to some other key
+            if (!hm.containsKey(ch1) && hm.containsValue(ch2)) {
+                return false;
+            }
+
+            //Add the pair to HashMap
+            hm.put(ch1, ch2);
+
+            System.out.println(hm);
+        }
+
+        return true;
+    }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        //Creating object of the class
+        InputStreamReader inp = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(inp);
+
+        //Taking user input
+        System.out.print("Enter the 1st string : ");
+        String str1 = b.readLine();
+        System.out.print("Enter the 2nd string : ");
+        String str2 = b.readLine();
+
+        //Checking if strings are isomorphic or not
+        System.out.print("isIsomorphic : " + isIsomorphic(str1, str2));
+    }
+}
